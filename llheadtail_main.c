ff2abc6dfc6635a30aab11c8a7ae9d57001da2a3
Lukas Krampitz <lkrampit@uoguelph.ca>
2023-10-05 10:48:53 -0400

    Add in the llheadonly code into llheadtail

--- llheadtail_main.c
+++ llheadtail_main.c
@@ -3,12 +3,207 @@
  * of the list, as we discussed in the notes.
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+
+#include "fasta.h"
+#include "LLvNode.h"
+
+/**
+ * Function to be passed in to llFree to delete allocated FATA record and its description and sequence
+ */
+void deleteFastaRecord(LLvNode *node, void *userdata)
+{
+	// check if there is a FASTA record here
+	if (node->value != NULL)
+	{
+		// deallocate the FASTA record
+		// do NOT deallocate the key b/c it is the description of the FASTA record and only needs to be freed once
+		fastaDeallocateRecord((FASTArecord *)node->value);
+	}
+}
+
+int processFasta(char *filename, double *timeTaken)
+{
+	FILE *fp;
+	FASTArecord fRecord;
+	int lineNumber = 0, recordNumber = 0, status;
+	int eofSeen = 0;
+	clock_t startTime, endTime;
+
+	LLvNode *headOnlyLL = NULL;			// the head of the Linked List that holds the records
+	LLvNode *newLLvNode = NULL;			// a pointer to a new node used in creation
+	FASTArecord *newFastaRecord = NULL; // a pointer to a new fasta record that can be used to instaticate new records before storing them in the Linked List
+
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		fprintf(stderr, "Failure opening %s : %s\n", filename, strerror(errno));
+		return -1;
+	}
+
+	/** record the time now, before we do the work */
+	startTime = clock();
+
+	do
+	{
+		/** print a '.' every 10,000 records so
+		 * we know something is happening */
+		if ((recordNumber % 10000) == 0)
+		{
+			printf(".");
+			fflush(stdout);
+		}
+
+		fastaInitializeRecord(&fRecord);
+
+		status = fastaReadRecord(fp, &fRecord);
+		if (status == 0)
+		{
+			eofSeen = 1;
+		}
+		else if (status > 0)
+		{
+			lineNumber += status;
+			recordNumber++;
+
+			// make a new record
+			newFastaRecord = fastaAllocateRecord(); // this also initializes it in this step
+
+			// now copy over the data
+			newFastaRecord->id = fRecord.id;
+			newFastaRecord->description = strdup(fRecord.description);
+			newFastaRecord->sequence = strdup(fRecord.sequence);
+
+			// make a new node
+			newLLvNode = llNewNode(newFastaRecord->description, (void *)newFastaRecord);
+
+			// append the new FASTA record to the LL
+			headOnlyLL = llAppend(headOnlyLL, newLLvNode);
+
+			// deallocate the strings in the buffer fRecord
+			fastaClearRecord(&fRecord);
+		}
+		else
+		{
+			fprintf(stderr, "status = %d\n", status);
+			fprintf(stderr, "Error: failure at line %d of '%s'\n", lineNumber, filename);
+			return -1;
+		}
+
+	} while (!eofSeen);
+	printf(" %d FASTA records\n", recordNumber);
+
+	/** record the time now, when the work is done,
+	 *  and calculate the difference*/
+	endTime = clock();
+
+	(*timeTaken) = ((double)(endTime - startTime)) / CLOCKS_PER_SEC;
+
+	fclose(fp);
+
+	// free all the memory that was malloc'd in this function
+	llFree(headOnlyLL, deleteFastaRecord, NULL);
+
+	return recordNumber;
+}
+
+int processFastaRepeatedly(
+	char *filename,
+	long repeatsRequested)
+{
+	double timeThisIterationInSeconds;
+	double totalTimeInSeconds = 0;
+	int minutesPortion;
+	int status;
+	long i;
+
+	for (i = 0; i < repeatsRequested; i++)
+	{
+		status = processFasta(filename, &timeThisIterationInSeconds);
+		if (status < 0)
+			return -1;
+		totalTimeInSeconds += timeThisIterationInSeconds;
+	}
+
+	printf("%.3lf seconds taken for processing total\n", totalTimeInSeconds);
+
+	totalTimeInSeconds /= (double)repeatsRequested;
+
+	minutesPortion = (int)(totalTimeInSeconds / 60);
+	totalTimeInSeconds = totalTimeInSeconds - (60 * minutesPortion);
+	printf("On average: %d minutes, %.3lf second per run\n",
+		   minutesPortion, totalTimeInSeconds);
+
+	return status;
+}
+
+void usage(char *progname)
+{
+	fprintf(stderr, "%s [<OPTIONS>] <file> [ <file> ...]\n", progname);
+	fprintf(stderr, "\n");
+	fprintf(stderr, "Prints timing of loading and storing FASTA records.\n");
+	fprintf(stderr, "\n");
+	fprintf(stderr, "Options: \n");
+	fprintf(stderr, "-R <REPEATS> : Number of times to repeat load.\n");
+	fprintf(stderr, "             : Time reported will be average time.\n");
+	fprintf(stderr, "\n");
+}
 
 /**
  * Program mainline
  */
 int main(int argc, char **argv)
 {
-	printf("To Do ....\n");
-	return 1;
+	int i, recordsProcessed = 0;
+	long repeatsRequested = 1;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i][0] == '-')
+		{
+			if (argv[i][1] == 'R')
+			{
+				if (i >= argc)
+				{
+					fprintf(stderr, "Error: need argument for repeats requested\n");
+					return 1;
+				}
+				if (sscanf(argv[++i], "%ld", &repeatsRequested) != 1)
+				{
+					fprintf(stderr, "Error: cannot parse repeats requested from '%s'\n", argv[i]);
+					return 1;
+				}
+			}
+			else
+			{
+				fprintf(stderr, "Error: unknown option '%s'\n", argv[i]);
+				usage(argv[0]);
+			}
+		}
+		else
+		{
+			recordsProcessed = processFastaRepeatedly(argv[i], repeatsRequested);
+			if (recordsProcessed < 0)
+			{
+				fprintf(stderr, "Error: Processing '%s' failed -- exitting\n", argv[i]);
+				return 1;
+			}
+			printf("%d records processed from '%s'\n",
+				   recordsProcessed, argv[i]);
+		}
+	}
+
+	if (recordsProcessed == 0)
+	{
+		fprintf(stderr,
+				"No data processed -- provide the name of"
+				" a file on the command line\n");
+		usage(argv[0]);
+		return 1;
+	}
+
+	return 0;
 }
